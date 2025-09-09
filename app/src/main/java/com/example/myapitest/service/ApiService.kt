9108666579317b1439758d5241260efec0d5a0c2
Julia Spader Bianchi <juulia.sbianchi@gmail.com>
2025-09-09 11:17:52 -0300

    ✨ fix(api): alterar retorno do método getCarById para CarDetail

--- app/src/main/java/com/example/myapitest/service/ApiService.kt
+++ app/src/main/java/com/example/myapitest/service/ApiService.kt
@@ -1,11 +1,12 @@
 package com.example.myapitest.service
 
 import com.example.myapitest.model.Car
+import com.example.myapitest.model.CarDetail
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ApiService {
 
     @GET("/car") suspend fun getCars(): List<Car>
-    @GET("/car/{id}") suspend fun getCarById(@Path("id") id: String): Car
+    @GET("/car/{id}") suspend fun getCarById(@Path("id") id: String): CarDetail
 }
