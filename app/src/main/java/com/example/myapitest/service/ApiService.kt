e9f5890aa0e80b0587a11fc81803f782e81beeee
Julia Spader Bianchi <juulia.sbianchi@gmail.com>
2025-09-09 14:56:55 -0300

    ✨ feat(ApiService): adicionar método deleteById

--- app/src/main/java/com/example/myapitest/service/ApiService.kt
+++ app/src/main/java/com/example/myapitest/service/ApiService.kt
@@ -2,6 +2,7 @@ package com.example.myapitest.service
 
 import com.example.myapitest.model.Car
 import com.example.myapitest.model.CarDetail
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -9,4 +10,6 @@ interface ApiService {
 
     @GET("/car") suspend fun getCars(): List<Car>
     @GET("/car/{id}") suspend fun getCarById(@Path("id") id: String): CarDetail
+    @DELETE("/car/{id}") suspend fun deleteById(@Path("id") id: String)
+
 }
