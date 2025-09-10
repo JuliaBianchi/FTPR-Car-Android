6b4521293800288799fc938628c437dd69ad3d11
Julia Spader Bianchi <juulia.sbianchi@gmail.com>
2025-09-10 17:04:33 -0300

    ✨ chore(NewItemActivity): limpar a formatação do código e remover a linha comentada

--- app/src/main/java/com/example/myapitest/NewItemActivity.kt
+++ app/src/main/java/com/example/myapitest/NewItemActivity.kt
@@ -1,4 +1,5 @@
 package com.example.myapitest
+
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -6,29 +7,31 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapitest.databinding.ActivityNewItemBinding
 
-
 class NewItemActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityNewItemBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // inicializar o binding
         binding = ActivityNewItemBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
         setupView()
+
     }
 
     private fun setupView() {
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
-        binding.toolbar.setNavigationOnClickListener { finish() }
+        binding.toolbar.setNavigationOnClickListener {
+            finish()
+        }
 
     }
 
     companion object {
         fun newIntent(context: Context) = Intent(context, NewItemActivity::class.java)
     }
+
 }
