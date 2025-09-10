255a998eb03832f32237dc88f84a3e486de8c1d4
Julia Spader Bianchi <juulia.sbianchi@gmail.com>
2025-09-10 14:41:03 -0300

    ✨ feat(NewItemActivity): criar nova activity para adicionar um novo item

---
+++ app/src/main/java/com/example/myapitest/NewItemActivity.kt
@@ -0,0 +1,34 @@
+package com.example.myapitest
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapitest.databinding.ActivityNewItemBinding
+
+
+class NewItemActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNewItemBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // inicializar o binding
+        binding = ActivityNewItemBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        setupView()
+    }
+
+    private fun setupView() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        binding.toolbar.setNavigationOnClickListener { finish() }
+
+    }
+
+    companion object {
+        fun newIntent(context: Context) = Intent(context, NewItemActivity::class.java)
+    }
+}
