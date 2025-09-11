9d8c63fefde6eeafc8613768f4f7c97d183de81e
Julia Spader Bianchi <juulia.sbianchi@gmail.com>
2025-09-11 11:50:50 -0300

    ✨ feat(retrofit-client): adicionar configuração do okHttpClient ao Retrofit

--- app/src/main/java/com/example/myapitest/service/RetrofitClient.kt
+++ app/src/main/java/com/example/myapitest/service/RetrofitClient.kt
@@ -11,6 +11,7 @@ object RetrofitClient {
      private val instance: Retrofit by lazy {
             Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
      }
