4a1852a6100b196f666722a56e30885d884a336b
Julia Spader Bianchi <juulia.sbianchi@gmail.com>
2025-09-11 11:08:21 -0300

    ✨ feat(NewItemActivity): integrar mapa para seleção de localização
    - implementar OnMapReadyCallback para configurar o mapa
    - adicionar funcionalidade de adicionar marcador ao clicar no mapa
    - obter e exibir a localização atual do dispositivo no mapa
    - solicitar permissão de localização fina (ACCESS_FINE_LOCATION)

--- app/src/main/java/com/example/myapitest/NewItemActivity.kt
+++ app/src/main/java/com/example/myapitest/NewItemActivity.kt
@@ -1,23 +1,60 @@
 package com.example.myapitest
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker
+import androidx.core.content.PermissionChecker.checkSelfPermission
 import com.example.myapitest.databinding.ActivityNewItemBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
-class NewItemActivity : AppCompatActivity() {
+class NewItemActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityNewItemBinding
+    private lateinit var mMap: GoogleMap
+    private var selectedMarker: Marker? = null
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNewItemBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
         setupView()
+        setupGoogleMap()
+    }
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        binding.mapContainer.visibility = View.VISIBLE
+        mMap.setOnMapClickListener {
+            latLng ->
+
+            // Limpar marcador anterior, se existir
+            selectedMarker?.remove()
+
+            // Adicionar novo marcador
+            selectedMarker = mMap.addMarker(
+                MarkerOptions().position(latLng).draggable(true).title("${latLng.latitude}, ${latLng.longitude}")
+            )
+        }
+        getDeviceLocation()
     }
 
     private fun setupView() {
@@ -27,11 +64,33 @@ class NewItemActivity : AppCompatActivity() {
         binding.toolbar.setNavigationOnClickListener {
             finish()
         }
+    }
 
+    private fun setupGoogleMap() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as com.google.android.gms.maps.SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    private fun getDeviceLocation() {
+        if (checkSelfPermission(  this, ACCESS_FINE_LOCATION) == PermissionChecker.PERMISSION_GRANTED) {
+            // Se já tem permissão, obter a localização atual
+            loadCurrentLocation()
+        } else {
+            // Se não tem permissão, solicitar permissão
+            // requestPermissions(arrayOf(ACCESS_FINE_LOCATION), 1)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun loadCurrentLocation() {
+        mMap.isMyLocationEnabled = true
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+            val currentLocationLatLng = LatLng(location.latitude, location.longitude)
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocationLatLng, 15f))
+        }
     }
 
     companion object {
         fun newIntent(context: Context) = Intent(context, NewItemActivity::class.java)
     }
-
 }
